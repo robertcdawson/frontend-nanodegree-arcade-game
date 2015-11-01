69ef2c68cd621ab9be116b35baad6c366398942c
Robert C Dawson <manatees@gmail.com>
2015-11-01 15:03:12 -0800

    Initial commit

--- js/app.js
+++ js/app.js
@@ -1,46 +1,214 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+// Create the Enemy object,
+// which is used to generate enemies.
+var Enemy = function(x, y) {
+  // Set the enemy's image and dimensions.
+  this.sprite = 'images/enemy-bug.png';
+  this.width = 100;
+  this.height = 70;
+  // Set the enemy's initial location.
+  this.x = x;
+  this.y = y;
+  // Set the enemy's speed.
+  var min = 50;
+  var max = 200;
+  var randSpeed = Math.random() * (max - min) + min;
+  this.speed = randSpeed;
 };
 
-// Update the enemy's position, required method for game
+// Update the enemy's position.
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+  // Update the Enemy's location.
+  var randPos = Math.floor(Math.random() * 3);
+  // If the enemy's x-coordinate exceeds 500 pixels,
+  // then move the enemy to the left side of the screen.
+  if (this.x > 500) {
+    // Move the enemy to (0 - Enemy's width) in pixels.
+    this.x = -this.width;
+    if (randPos === 0) {
+      this.y = 55;
+    } else if (randPos === 1) {
+      this.y = 140;
+    } else {
+      this.y = 225;
+    }
+    // Else, move the enemy across the screen.
+    // Multiply any movement by the dt parameter
+    // to ensure the game runs at the same speed
+    // on all computers.
+  } else {
+    this.x += (this.speed * dt);
+  }
+
+  // Handle collision with the player.
+  if (player.x >= this.x - (this.width / 2) &&
+    player.x <= this.x + (this.width / 2) &&
+    player.y >= this.y - (this.height / 2) &&
+    player.y <= this.y + (this.height / 2)) {
+    player.reset();
+    if (player.score < 10) {
+      player.removeLives();
+    }
+  }
+
 };
 
-// Draw the enemy on the screen, required method for game
+// Draw the enemy on the screen.
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+// Create the Player object,
+// which is used to generate the player.
+var Player = function(x, y) {
+  // Load the player's image by setting this.sprite
+  // to the appropriate image in the image folder.
+  this.sprite = 'images/char-horn-girl.png';
+  // Set the player's initial location.
+  this.x = x;
+  this.y = y;
+};
 
+// Update the player's position.
+// Parameter: dt, a time delta between ticks
+Player.prototype.update = function() {
+  this.moveLeft = this.x - 50;
+  this.moveRight = this.x + 50;
+  this.moveUp = this.y - 50;
+  this.moveDown = this.y + 50;
+};
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+// Draw the player on the screen.
+Player.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  // player.showScore();
+  // player.showLives();
+  player.showLivesAndScore();
+};
 
+// Move the player back to his or her initial position.
+Player.prototype.reset = function() {
+  this.x = 200;
+  this.y = 400;
+};
 
+// Create the score.
+Player.prototype.initScore = function() {
+  this.score = 0;
+};
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+// Increment the score.
+Player.prototype.addScore = function() {
+  this.score += 1;
+};
+
+// Create the life count.
+Player.prototype.initLives = function() {
+  this.lives = 5;
+};
+
+// Decrement the life count until it reaches 0.
+Player.prototype.removeLives = function() {
+  this.lives = (this.lives > 0) ? this.lives -= 1 : this.lives = 0;
+};
+
+Player.prototype.showLivesAndScore = function() {
+  ctx.font = "16px sans-serif";
+  // If the player has lives, show the score and life count.
+  if (player.lives > 0) {
+    // If the score is less than 10, show the score and life count.
+    if (player.score < 10) {
+      ctx.fillStyle = "white";
+      ctx.fillText("Score: " + player.score + "/10", 7, 572);
+      ctx.fillStyle = "yellow";
+      ctx.fillText("Lives: " + player.lives + "/5", 415, 572);
+    }
+    // Else, the show winning notification.
+    else {
+      ctx.fillStyle = "yellow";
+      ctx.font = "20px sans-serif";
+      ctx.fillText("You Win!", 212, 572);
+    }
+  }
+  // Else, show the losing notification.
+  else {
+    // If the score is less than 10, show losing notification.
+    // Else, show nothing.
+    if (player.score < 10) {
+      ctx.fillStyle = "yellow";
+      ctx.font = "20px sans-serif";
+      ctx.fillText("Game Over", 202, 572);
+      player.score = 0;
+    }
+  }
+};
+
+// Move the player according to allowedKeys input.
+Player.prototype.handleInput = function(allowedKeys) {
+  // Left key should move the player to the left,
+  // right key to the right,
+  // up should move the player up and
+  // down should move the player down.
+  // Check for off-screen movement and handle appropriately.
+  if (allowedKeys === 'left') {
+    if (this.x >= 50) {
+      // this.x = this.x - 50;
+      this.x = this.moveLeft;
+    } else {
+      this.x = this.x;
+    }
+  } else if (allowedKeys === 'right') {
+    if (this.x <= 350) {
+      this.x = this.moveRight;
+    } else {
+      this.x = this.x;
+    }
+  } else if (allowedKeys === 'up') {
+    console.log(this.y);
+    if (this.y >= 50) {
+      this.y = this.moveUp;
+    } else {
+      // If the player reaches the water,
+      // the game should be reset by moving the player
+      // back to his or her initial position.
+      if (this.y === 0 && this.x === 200) {
+        player.addScore();
+      } else {
+        player.removeLives();
+      }
+      this.reset();
+    }
+  } else if (allowedKeys === 'down') {
+    if (this.y <= 350) {
+      this.y = this.moveDown;
+    } else {
+      this.y = this.y;
+    }
+  }
+};
+
+// Place all enemy objects in an array called allEnemies.
+var enemy1 = new Enemy(0, 55);
+var enemy2 = new Enemy(100, 140);
+var enemy3 = new Enemy(200, 225);
+var allEnemies = [enemy1, enemy2, enemy3];
+
+// Place the player object in a variable called player.
+var player = new Player(200, 400);
+
+// Initialize the score variable.
+player.initScore();
+player.initLives();
+
+// This listens for key presses and sends the keys
+// to the Player.handleInput() method.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
-
-    player.handleInput(allowedKeys[e.keyCode]);
+  var allowedKeys = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down'
+  };
+  // Call the handleInput method.
+  player.handleInput(allowedKeys[e.keyCode]);
 });
